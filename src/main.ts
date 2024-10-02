fea846db1ceb163936cab383cd5161c501d5a408
Dagadev <gacub20ru@gmail.com>
2024-10-02 16:26:42 -0600

    Add api/v1 prefix

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ import { ValidationPipe } from '@nestjs/common'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.setGlobalPrefix('api/v1')
   app.enableCors()
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true
-  }))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true
+    })
+  )
 
-  await app.listen(process.env.PORT ?? 4000);
+  await app.listen(process.env.PORT ?? 4000)
 }
 
 bootstrap()
