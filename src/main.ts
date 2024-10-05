bc40d791372ff7887f612d238991c081c0ae953c
Dagadev <gacub20ru@gmail.com>
2024-10-05 12:34:14 -0600

    User global prisma filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import { PrismaFilter } from './prisma/prisma.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.useGlobalFilters(new PrismaFilter())
   app.setGlobalPrefix('api/v1')
   app.enableCors()
   app.useGlobalPipes(
