4b4d4b680c26b2f7597514e7b17a2fad114b16fd
Dagadev <gacub20ru@gmail.com>
2024-10-05 22:02:31 -0600

    Add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
 import { LogsModule } from './logs/logs.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, LogsModule]
+  imports: [PrismaModule, AuthModule, UsersModule, LogsModule, ThrottlerModule.forRoot([
+    {
+      ttl: 1000,
+      limit: 1
+    }
+  ])]
 })
 export class AppModule {}
