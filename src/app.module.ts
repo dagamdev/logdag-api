a89781b7045a570a55c50ff1bae625ad4f0b3bf5
Dagadev <gacub20ru@gmail.com>
2024-10-07 11:04:28 -0600

    Format code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,22 @@
 import { Module } from '@nestjs/common'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
-import { LogsModule } from './logs/logs.module';
-import { ThrottlerModule } from '@nestjs/throttler';
-import { PrismaModule } from './prisma/prisma.module';
+import { LogsModule } from './logs/logs.module'
+import { ThrottlerModule } from '@nestjs/throttler'
+import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule, LogsModule, ThrottlerModule.forRoot([
-    {
-      ttl: 1000,
-      limit: 1
-    }
-  ])]
+  imports: [
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    LogsModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 1000,
+        limit: 1
+      }
+    ])
+  ]
 })
 export class AppModule {}
